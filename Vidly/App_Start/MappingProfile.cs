5fef475e191aaa7d82b711668264196072e2945e
mperella <m_perella@hotmail.com>
2017-08-26 11:21:39 -0400

    Added Web APIs

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>()
+                .ForMember(m => m.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<CustomerDto, Customer>();
+
+            Mapper.CreateMap<Movie, MovieDto>()
+                .ForMember(m => m.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>();
+
+
+        }
+    }
+}
--- Vidly/Dtos/MovieDto.cs
+++ Vidly/Dtos/MovieDto.cs
@@ -1,32 +1,24 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace Vidly.Models
+namespace Vidly.Dtos
 {
-    public class Movie
+    public class MovieDto
     {
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
 
-        public Genre Genre { get; set; }
-
-
-        [Display(Name = "Genre")]
         public int GenreId { get; set; }
 
         [Required]
-        [Display(Name = "Release Date")]
         public DateTime? ReleaseDate { get; set; }
 
         [Required]
-        [Display(Name = "Date Added")]
         public DateTime? AddDate { get; set; }
 
         [Required]
         [Range(1, 20)]
-        [Display(Name = "Number In Stock")]
         public int? NumberInStock { get; set; }
-
     }
 }
